cd8d18d941b0ecc3818b144cd166e0178c1ac5db
Ricard Renalias <ricard.renalias@estudiantat.upc.edu>
2025-04-19 20:42:13 +0200

    Select stt languange as Catalan to improve acuracy in Catalan

--- src/utils/stt.py
+++ src/utils/stt.py
@@ -4,7 +4,7 @@ from . import record
 
 # Possible models: tiny, base, small, medium, large, turbo
 # More info and requirements: https://pypi.org/project/openai-whisper/
-def stt(audio_file, whisper_model="turbo"):
+def stt(audio_file, whisper_model="turbo", language="ca"):
     model = whisper.load_model(whisper_model)
-    transcription =  model.transcribe(audio_file)["text"]
+    transcription =  model.transcribe(audio_file, language=language)["text"]
     return transcription
